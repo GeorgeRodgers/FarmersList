9319e26b917e7804e1df8115f863c5a6e736f44d
George Rodgers <george.rodgers.chem@gmail.com>
2024-09-30 16:07:31 +0100

    project complete, all tests pass

---
+++ classes/Auth.js
@@ -0,0 +1,21 @@
+const Customer = require("./Customer");
+
+class Auth{
+    constructor(){
+        this.customers = [];
+    }
+    register(name, email, shippingAddress){
+        this.customers.push(new Customer(name, email, shippingAddress));
+    }
+    login(email){
+        for (let i = 0; i < this.customers.length; i++){
+            if (email === this.customers[i].email){
+                return this.customers[i];
+            } else {
+                return null
+            }
+        }
+    }
+}
+
+module.exports = Auth;
